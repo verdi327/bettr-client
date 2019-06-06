8d6a61a81807d349951506a04cd421486f4c04e0
Michael Verdi <michael.v.verdi@gmail.com>
2019-06-06 18:30:10 -0400

    updating deployment stuff

--- src/config.js
+++ src/config.js
@@ -1,3 +1,10 @@
+// let apiPath;
+// if (process.env.NODE_ENV === 'production') {
+//   apiPath = 'https://bettr-fit-api.herokuapp.com/api'
+// } else {
+//   apiPath = 'http://localhost:8000/api'
+// }
+
 export default {
   API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
   TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'bettr-client-auth-token',
