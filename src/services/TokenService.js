5ba8515205a38c082d5851bf89b797f9decdee70
Michael Verdi <michael.v.verdi@gmail.com>
2019-06-14 13:54:20 -0400

    switch from session to local storage

--- src/services/TokenService.js
+++ src/services/TokenService.js
@@ -2,13 +2,13 @@ import config from '../config'
 
 const TokenService = {
   saveAuthToken(token) {
-    window.sessionStorage.setItem(config.TOKEN_KEY, token)
+    window.localStorage.setItem(config.TOKEN_KEY, token)
   },
   getAuthToken() {
-    return window.sessionStorage.getItem(config.TOKEN_KEY)
+    return window.localStorage.getItem(config.TOKEN_KEY)
   },
   clearAuthToken() {
-    window.sessionStorage.removeItem(config.TOKEN_KEY)
+    window.localStorage.removeItem(config.TOKEN_KEY)
   },
   hasAuthToken() {
     return !!TokenService.getAuthToken()
